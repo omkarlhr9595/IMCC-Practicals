81cf90d05bdbdd66f6d3a1e173879bc33c8013b0
Omkar Lohar <omkarlohar91@gmail.com>
2022-12-15 14:22:09 +0530

    Nothing

---
+++ FY/Java/Assi1.java
@@ -0,0 +1,22 @@
+public class Assi1 {
+
+    int var1 = 100;
+    int var2 = 200;
+
+    public static void method1() {
+        Assi1 obj = new Assi1();
+        System.out.println(obj.var1);
+        System.out.println(obj.var2);
+    }
+
+    public static void method2() {
+        Assi1 obj = new Assi1();
+        System.out.println(obj.var1);
+        System.out.println(obj.var2);
+    }
+
+    public static void main(String[] args) {
+        Assi1.method1();
+        Assi1.method2();
+    }
+}
