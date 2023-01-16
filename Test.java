083456f66546441a77bd6123916311d83eae344a
Omkar Lohar <omkarlohar91@gmail.com>
2023-01-16 22:02:53 +0530

    Files added

--- Test.java
+++ Test.java
@@ -1,12 +1,32 @@
-abstract class A {
-    void display() {
-        System.out.println("Good Afternoon");
+
+class Fruit {
+
+}
+
+class Mango extends Fruit {
+
+}
+
+class Banana extends Mango {
+
+}
+
+class A {
+    Fruit building() {
+        System.out.println("1 floor");
+
+        Fruit f = new Fruit();
+
+        return f;
+
     }
 
-    abstract void display2();
 }
 
-abstract class B extends A{
- 
-}
+class B extends A {
 
+    Banana building() {
+        System.out.println("2 floor");
+        return new Banana();
+    }
+}
